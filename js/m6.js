45938303089979030a9b5c12c29c227cb6575430
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-02 22:13:53 +0200

    Update m6.js

--- js/m6.js
+++ js/m6.js
@@ -52,9 +52,11 @@ const books=[
     какими инструментами ему нужно пользоваться.`,
 	},
 ];
-
-localStorage.setItem("books", JSON.stringify(books))
-
+const booksFromLS = localStorage.getItem("books");
+// console.log(booksFromLS);
+if (booksFromLS === null) {
+    localStorage.setItem("books", JSON.stringify(books))
+}
 const divEl = document.querySelector("#root");
 const div1 = document.createElement("div");
 const div2 = document.createElement("div");
@@ -75,19 +77,19 @@ btn.textContent = "Add";
 
 div1.append(title, list, btn);
 
-const listBooks = document.querySelector("ul");
+const bookList = document.querySelector("ul");
 const btnAdd = document.querySelector(".btn__add")
 btnAdd.addEventListener('click', addBook)
 
 function renderList() {
 const books=JSON.parse(localStorage.getItem('books'))
-    const bookMarkApp = books.map(book => {
+    const bookMarkup = books.map(book => {
         return `<li class="item" id=${book.id}>
         <p class="book__title">${book.title}</p>
         <button class="btn__del">Delete</button>
         <button class="btn__ed">Edit</button></li>`
     }).join("")
-    listBooks.insertAdjacentHTML("afterbegin", bookMarkApp);
+    bookList.insertAdjacentHTML("afterbegin", bookMarkup);
 
     document.querySelectorAll(".book__title").forEach(element => {
         element.addEventListener("click", renderPreview)
@@ -105,37 +107,46 @@ const books=JSON.parse(localStorage.getItem('books'))
 renderList();
 
 function renderPreview(e) {
+const books = JSON.parse(localStorage.getItem("books"));
     const curBook = books.find(book=>book.title===e.currentTarget.textContent)
     // console.log(book);
-    renderMarkup(curBook);
-    div2.insertAdjacentHTML("afterbegin", renderMarkup(curBook));
+    renderBookMarkup(curBook);
+    div2.innerHTML=''
+    div2.insertAdjacentHTML("afterbegin", renderBookMarkup(curBook));
 }
-function renderMarkup(book) {
-    div2.innerHTML=""
+
+function renderBookMarkup(book) {
+    // div2.innerHTML=""
     return `<h2>${book.title}</h2>
     <p>${book.author}</p>
     <img src="${book.img}" alt="${book.title}">
     <p>${book.plot}</p>`
 }
+
 function renderButtonDel(e) {
-    // console.log(e.target.parentNode);
-    // console.log(e.target.parentNode.id);
-    
-    const curIndex = books.findIndex(book => e.target.parentNode.id === book.id)
+    const books = JSON.parse(localStorage.getItem("books"));
+    console.log(e.target.parentNode.id);
+    const curIndex = books.findIndex((book) => book.id === e.target.parentNode.id)
     console.log(curIndex);
-    const renderPrevEl = div2.querySelector("h2").textContent;
-    const renderTitleEl = div2.querySelector("h2");
-    if (renderTitleEl) {
+
+    const renderPrevEl = div2.querySelector("h2");
+     console.log(renderPrevEl);
+    
+   
+    
+    
+    if (renderPrevEl) {
+        const renderTitleEl = div2.querySelector("h2").textContent;
+        console.log(renderTitleEl);
         console.log(books[curIndex].title);
-    if (books[curIndex].title === renderPrevEl) {
+    if (books[curIndex].title === renderTitleEl) {
         div2.innerHTML=""
     } }
 
     // books.splice(curIndex, 1);    
-    const updatedbooks = books.filter(book => {
-        if (book !==)
-    })
-    list.innerHTML = "";
+    bookList.innerHTML=''
+    const updatedBooks = books.filter((book) => e.target.parentNode.id !== book.id);
+  localStorage.setItem("books", JSON.stringify(updatedBooks));
     renderList()
    
 }
@@ -144,24 +155,40 @@ function renderButtonEdit(e) {
 }
 
 function addBook(e) {
-    const newBook = { id: `{Date.now}`, title: '', author: '', img: '', plot: '' }
-    div2.innerHTML = '';
+    const newBook = {
+        id: `${Date.now()}`,
+        title: '',
+        author: '',
+        img: '',
+        plot: '',
+    };
+    // div2.innerHTML = '';
     div2.insertAdjacentHTML('afterbegin', renderFormMarkup());
+
     formFunction(newBook);
+
     const btnSubmit = document.querySelector(".submit");
+
     btnSubmit.addEventListener('click', saveChanges);
+
+
     function saveChanges(e) {
+        console.log(newBook);
         console.log(newBook.title);
         console.log(newBook.author);
         console.log(newBook.img);
         console.log(newBook.plot);
         if (newBook.title && newBook.author && newBook.img && newBook.plot) {
+            const books = JSON.parse(localStorage.getItem("books"));
+            console.log(books);
+            console.log(newBook);
             books.push(newBook);
-            list.innerHTML = '';
+            localStorage.setItem("books", JSON.stringify(books));
+            bookList.innerHTML = '';
             renderList();
-            renderMarkup(newBook);
+            // renderBookMarkup(newBook);
             div2.innerHTML=""
-            div2.insertAdjacentHTML("afterbegin", renderMarkup(newBook));
+            div2.insertAdjacentHTML("afterbegin", renderBookMarkup(newBook));
         }
         console.log(newBook);
         console.log(books);
@@ -171,24 +198,25 @@ function addBook(e) {
 function renderFormMarkup() {
     return `<form action="">
   <label for="">Name
-    <input type="text">
+    <input type="text" name="title">
   </label>
   <label for="">Author
-    <input type="text">
+    <input type="text" name="author">
   </label>
   <label for="">Image
-    <input type="text">
+    <input type="text" name="img">
   </label>
-  <label for="">Plot
-    <input type="text">
-  </label>
-  <button type ='button' class='submit'>Add</button>
+  <label for="">plot
+    <input type="text" name="plot">
+    <button type ='button' class='submit'>Add</button>
+  </label>  
 </form>`
 }
 
 function formFunction(book) {
-    document.querySelectorAll('input').forEach(el=>el.addEventListener('change',fillObject))
+    document.querySelectorAll('input').forEach((el) =>
+        el.addEventListener('change', fillObject));
     function fillObject(e) {        
-        book[e.target.name]=e.target.value
+        book[e.target.name] = e.target.value;
 }
 }
